ed75b76c0bb1cc6f46f8defc7632274a18fdb6d3
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-11 10:24:23 -0300

    Create Mad_Lib_Desafio_1.rb

---
+++ Mad_Lib/Mad_Lib_Desafio_1.rb
@@ -0,0 +1,20 @@
+print "Tell me a pet: "
+pet = gets.chomp
+
+print "Do you have a pet? "
+have_pet = gets.chomp
+
+print "Tell me a verb: "
+verb = gets.chomp
+
+print "Tell me a color: "
+color = gets.chomp
+
+print "Tell me an adverb: "
+adverb = gets.chomp
+
+if have_pet
+  puts "This weekend I am going camping with my #{color} #{pet}. I am so #{adverb} to #{verb} in a tent. That's fun!"
+else
+  puts "This weekend I am going camping with my uncle's #{color} #{pet}. I am so #{adverb} to #{verb} in a tent. That's fun!"
+end
