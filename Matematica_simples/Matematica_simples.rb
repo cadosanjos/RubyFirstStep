c1582aa50880b8b284cb7bee887ea9641f3a5bf4
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-12 20:37:44 -0300

    Create Matematica_simples.rb

---
+++ Matematica_simples/Matematica_simples.rb
@@ -0,0 +1,35 @@
+def get_numbers
+
+  begin
+    print "What's the number? "
+    number = Integer(gets.chomp)
+  rescue
+    puts "Please enter an integer number"
+    retry
+  end
+end
+
+def sum (one, two)
+  sum = one + two
+end
+
+def difference (one, two)
+  difference = one - two
+end
+
+def product (one, two)
+  product = one * two
+end
+
+def quotient (one, two)
+  if two.zero?
+    quotient = "It's not possible to made a zero division"
+  else
+    quotient = one / two
+  end
+end
+
+number_one = get_numbers
+number_two = get_numbers
+
+puts "#{number_one} + #{number_two} = #{sum(number_one, number_two)}\n#{number_one} - #{number_two} = #{difference_result = difference(number_one, number_two)}\n#{number_one} * #{number_two} = #{product_result = product(number_one, number_two}\n#{number_one} / #{number_two} = #{quotient_result = quotient(number_one, number_two)}"
