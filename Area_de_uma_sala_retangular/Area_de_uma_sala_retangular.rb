645b6013a6eaa61b962af3bdf5f5101071f0659e
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 13:01:21 -0300

    typo fix and begin removed

--- Area_de_uma_sala_retangular/Area_de_uma_sala_retangular.rb
+++ Area_de_uma_sala_retangular/Area_de_uma_sala_retangular.rb
@@ -1,21 +1,18 @@
-def create_entries(entrie)
-
-  begin
-    print "What is the #{entrie} of the room in feet? "
-    number = Float(gets.chomp)
-  rescue
-    puts "Please enter a number"
-    retry
-  end
+def create_entries(entry)
+  print "What is the #{entry} of the room in feet? "
+  number = Float(gets.chomp)
+rescue
+  puts "Please entry a number"
+  retry
 end
 
-lenght_feet = create_entries("lenght")
+length_feet = create_entries("length")
 width_feet = create_entries("width")
 
-feet_squared = lenght_feet * width_feet
+feet_squared = length_feet * width_feet
 meters_squared = feet_squared * 0.09290304
 
-puts "You entered dimensions of #{lenght_feet} feet by #{width_feet} feet."
+puts "You entered dimensions of #{length_feet} feet by #{width_feet} feet."
 puts "The area is:"
 puts "#{feet_squared} square feet"
 puts "#{meters_squared} square meters"
