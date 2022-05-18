84aafac6e47b181254220b0bf979e505172957d5
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 14:23:50 -0300

    Update Mad_Lib.rb

--- Mad_Lib/Mad_Lib.rb
+++ Mad_Lib/Mad_Lib.rb
@@ -1,13 +1,13 @@
-print "Tell me a noun: "
+print 'Tell me a noun: '
 noun = gets.chomp
 
-print "A verb: "
+print 'A verb: '
 verb = gets.chomp
 
-print "An adjective: "
+print 'An adjective: '
 adjective = gets.chomp
 
-print "An adverb: "
+print 'An adverb: '
 adverb = gets.chomp
 
 puts "Do you #{verb} with your #{adjective} #{noun} #{adverb}? That's fun!"
