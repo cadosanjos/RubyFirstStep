47ff159f3f7176b0453dfc02804f26f82d641d99
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 13:34:04 -0300

    Update Desafio.rb

--- Area_de_uma_sala_retangular/Desafio.rb
+++ Area_de_uma_sala_retangular/Desafio.rb
@@ -1,44 +1,38 @@
-
-
-def create_entries(entrie, unit)
-
-  begin
-    print "What is the #{entrie} of the room in #{unit}? "
-    number = Float(gets.chomp)
-    return number
-  rescue
-    puts "Please enter an integer number"
-    retry
-  end
+def create_entries(entry, unit)
+  print "What is the #{entry} of the room in #{unit}? "
+  Float(gets.chomp)
+rescue
+  puts 'Please enter an integer number'
+  retry
 end
 
 def validate_unit
   print "Room area calculate \nWhat is the unit you want to work with? Feets or Meters? "
   unit = gets.chomp.downcase
-  if unit != "feets" and unit != "meters"
-    puts "Invalid Unit"
+  if unit != 'feets' && unit != 'meters'
+    puts 'Invalid Unit'
     exit
   end
   return unit
 end
 
-unit = validate_unit()
-lenght = create_entries("lenght", unit)
-width = create_entries("width", unit)
+unit = validate_unit
+length = create_entries('length', unit)
+width = create_entries('width', unit)
 
-if unit == "feets"
-  squared = lenght * width
+if unit == 'feets'
+  squared = length * width
   print squared
   converted_squared = squared * 0.09290304
   print converted_squared
-  converted_squared_unit = "meters"
+  converted_squared_unit = 'meters'
 else
-  squared = lenght * width
+  squared = length * width
   converted_squared = squared / 0.09290304
-  converted_squared_unit = "feet"
+  converted_squared_unit = 'feet'
 end
 
-puts "You entered dimensions of #{lenght} feet by #{width} #{unit}."
-puts "The area is:"
+puts "You entered dimensions of #{length} feet by #{width} #{unit}."
+puts 'The area is:'
 puts "#{squared} square #{unit}"
 puts "#{converted_squared} square #{converted_squared_unit}"
