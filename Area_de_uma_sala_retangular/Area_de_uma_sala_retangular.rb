2fe10179205f62d56985fecbdcbfe2e155f3eda0
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 14:48:37 -0300

    Update Area_de_uma_sala_retangular.rb

--- Area_de_uma_sala_retangular/Area_de_uma_sala_retangular.rb
+++ Area_de_uma_sala_retangular/Area_de_uma_sala_retangular.rb
@@ -2,7 +2,7 @@ def create_entries(entry)
   print "What is the #{entry} of the room in feet? "
   Float(gets.chomp)
 rescue
-  puts "Please entry a number"
+  puts 'Please entry a number'
   retry
 end
 
@@ -13,6 +13,6 @@ feet_squared = length_feet * width_feet
 meters_squared = feet_squared * 0.09290304
 
 puts "You entered dimensions of #{length_feet} feet by #{width_feet} feet."
-puts "The area is:"
+puts 'The area is:'
 puts "#{feet_squared} square feet"
 puts "#{meters_squared} square meters"
