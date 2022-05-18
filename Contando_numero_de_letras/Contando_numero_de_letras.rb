6625a3cf510644add56a3af913277da2d653d9de
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 13:40:11 -0300

    Update Contando_numero_de_letras.rb

--- Contando_numero_de_letras/Contando_numero_de_letras.rb
+++ Contando_numero_de_letras/Contando_numero_de_letras.rb
@@ -2,7 +2,7 @@ print "What's the input string? "
 written_string = gets.chomp
 
 if written_string.empty?
-  puts "Please, entry a word to run the programm"
+  puts 'Please, entry a word to run the programm'
 else
   puts "#{written_string} has #{written_string.length} characters."
 end
