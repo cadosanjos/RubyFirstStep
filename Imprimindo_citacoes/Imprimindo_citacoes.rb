a2dfd3ac22369988ea8efd09f8307863a41e1c89
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2022-05-18 14:18:27 -0300

    Update Imprimindo_citacoes.rb

--- Imprimindo_citacoes/Imprimindo_citacoes.rb
+++ Imprimindo_citacoes/Imprimindo_citacoes.rb
@@ -1,11 +1,11 @@
 print "What's the quote? "
 quote = gets.chomp
 
-print "Who said it? "
+print 'Who said it? '
 author = gets.chomp
 
 if quote.empty? or author.empty?
-  puts "Please, entry a quote and a author to run the programm"
+  puts 'Please, entry a quote and a author to run the programm'
 else
   puts author + ' says, "' + quote + '"'
 end
